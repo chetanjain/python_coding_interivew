9e9a94f66f669ed90247d93ad5898a8b5932f1b7
chetan jain <ldh.chetan@gmail.com>
2024-06-26 19:27:14 +0530

    Create Int_Str_separate.py

---
+++ Int_Str_separate.py
@@ -0,0 +1,16 @@
+# write a code to separate out integers and strings and sum the integers.
+# Examples
+# st = "50GS50HH100HT50GG" -> 50,50,100,50 and sum will be 250
+
+data=[]
+single_var='0'
+for index,i in enumerate(st):
+  if i.isdigit():
+    single_var = single_var+i
+    if len(st)==index+1:
+      data.append(int(single_var))
+  else:
+    data.append(int(single_var))
+    single_var = '0'
+print(data)
+print(sum(data))
