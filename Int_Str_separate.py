537a99f19e309d57a0bc86c5b47c451514e6f487
chetan jain <ldh.chetan@gmail.com>
2024-09-10 11:16:27 +0530

    Update Int_Str_separate.py

--- Int_Str_separate.py
+++ Int_Str_separate.py
@@ -2,6 +2,15 @@
 # Examples
 # st = "50GS50HH100HT50GG" -> 50,50,100,50 and sum will be 250
 
+# option 1
+import re
+pattern = r'\d+'
+st = "50GS50HH100HT50GG" 
+data = re.findall(pattern,st)
+print(data)
+
+#OR Option 2
+
 data=[]
 single_var='0'
 for index,i in enumerate(st):
